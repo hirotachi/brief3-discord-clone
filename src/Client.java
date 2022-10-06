f66226f2f952bfcc7176a84e44bc21007ce6db41
Said Oudouane <saidoudouane@gmail.com>
2022-10-06 11:20:58 +0100

    add ability to stream files

--- src/Client.java
+++ src/Client.java
@@ -1,6 +1,8 @@
 import java.io.*;
+import java.lang.reflect.Array;
 import java.net.Socket;
-import java.util.List;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Client {
@@ -9,6 +11,7 @@ public class Client {
     private BufferedReader bufferedReader;
     private BufferedWriter bufferedWriter;
     private String username;
+
 
     public Client(Socket socket, String username) {
         try {
@@ -32,13 +35,96 @@ public class Client {
             while (socket.isConnected()) {
                 System.out.print(">> ");
                 String message = scanner.nextLine();
-                bufferedWriter.write(username + ": " + message);
+                if (message.startsWith("/upload")) {
+                    uploadFile(message);
+                }
+                else if (message.startsWith("/list")){
+                    listFilesInCurrentDirectory();
+                } else if (message.startsWith("/exit")){
+                    System.out.println("Closing connection...");
+                    closeLeaks(socket, bufferedReader, bufferedWriter);
+                    System.exit(0);
+                    return;
+                }
+                else {
+                    bufferedWriter.write(username + ": " + message);
+                }
+
+
                 bufferedWriter.newLine();
                 bufferedWriter.flush();
             }
         } catch (IOException e) {
             closeLeaks(socket, bufferedReader, bufferedWriter);
         }
+    }
+
+    private void uploadFile(String command) {
+//        /upload filePath
+        String[] commandParts = command.split(" ");
+        String filePath = commandParts[1];
+        File file = new File(filePath);
+        if (!file.exists()) {
+            System.out.println("File" + filePath + " does not exist!");
+            return;
+        }
+
+//         convert the whole file to bytes and then send it
+        try {
+            byte[] fileBytes = new byte[(int) file.length()];
+            FileInputStream fileInputStream = new FileInputStream(file);
+            fileInputStream.read(fileBytes);
+
+            bufferedWriter.write("/upload " + file.getName() + " " + Arrays.toString(fileBytes));
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+            fileInputStream.close();
+            System.out.println("File " + file.getName() + " sent successfully!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void downloadFile(String receivedMsg) {
+//         /upload filename fileBytes(bytes)
+//         while not done keep appending to downloading file in hashmap
+//         when done, write to file
+        String[] commandParts = receivedMsg.split(" ");
+        String fileName = commandParts[1];
+        String fileBytes = String.join(" ", Arrays.copyOfRange(commandParts, 2, commandParts.length));
+        String downloadPath = System.getProperty("user.dir") + File.separator + "download" + File.separator + fileName;
+        System.out.println("Downloading " + downloadPath + "...");
+//         check if download folder exists and create if not
+        File downloadFolder = new File(System.getProperty("user.dir") + File.separator + "download");
+        if (!downloadFolder.exists()) downloadFolder.mkdir();
+
+//         convert filesBytes to bytes array and then save to file in download folder
+        File file = new File(downloadPath);
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            System.out.println("Could not create file " + downloadPath);
+            return;
+        }
+        byte[] fileBytesArray = convertStringToByteArray(fileBytes);
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            fileOutputStream.write(fileBytesArray);
+            fileOutputStream.close();
+            System.out.println("Downloaded " + downloadPath + "!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private byte[] convertStringToByteArray(String fileBytes) {
+        String[] fileBytesStringArray = fileBytes.substring(1, fileBytes.length() - 1).split(", ");
+        byte[] fileBytesArray = new byte[fileBytesStringArray.length];
+        for (int i = 0; i < fileBytesStringArray.length; i++) {
+            fileBytesArray[i] = Byte.parseByte(fileBytesStringArray[i]);
+        }
+        return fileBytesArray;
     }
 
     public void receiveMsg() {
@@ -49,6 +135,10 @@ public class Client {
                 while (socket.isConnected()) {
                     try {
                         receivedMsg = bufferedReader.readLine();
+                        if (receivedMsg.startsWith("/upload")) {
+                            downloadFile(receivedMsg);
+                            continue;
+                        }
                         System.out.println(receivedMsg);
                     } catch (IOException e) {
                         closeLeaks(socket, bufferedReader, bufferedWriter);
@@ -56,6 +146,14 @@ public class Client {
                 }
             }
         }).start();
+    }
+
+    private void listFilesInCurrentDirectory() {
+        File currentDirectory = new File(System.getProperty("user.dir"));
+        File[] files = currentDirectory.listFiles();
+        for (File file : files) {
+            System.out.println(file.getName());
+        }
     }
 
     private void closeLeaks(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
@@ -79,13 +177,12 @@ public class Client {
         boolean hasArgs = args.length > 0;
         String host = "localhost";
         int port = 6667;
-        if(hasArgs) {
+        if (hasArgs) {
             host = args[0];
             port = Integer.parseInt(args[1]);
-        }else{
+        } else {
             System.out.println("No args provided, using default host and port");
         }
-
 
 
         Scanner scanner = new Scanner(System.in);
